8a7b6da874581196915ee858139b6ebe64621d15
Yashwanth K <yashwanth626@gmail.com>
2025-01-07 12:47:29 +0530

    07-01-2025 Push

--- Os Module.py
+++ Os Module.py
@@ -27,6 +27,19 @@ for root,directory,file in os.walk(os.getcwd()):
     for f in file:
         if '.txt' in f:
          print(root+'\\'+f)
+
+'''Arguments used along with os.walk(): 
+                 os.walk(top, topdown=True, onerror=None, followlinks=False)
+1) top: This is the root directory from which the walk begins. It's a string representing the path to the starting directory.
+2) topdown (Optional, Default: True): This boolean parameter controls the order in which directories are visited.
+      True: The walk starts at the top directory and visits its subdirectories recursively (pre-order traversal). This is the default.
+      False: The walk visits subdirectories first and then the top directory (post-order traversal).
+3) onerror (Optional, Default: None): This is a function that will be called if an error occurs during the walk (e.g., permission denied). 
+   The function receives an OSError instance as its argument. If onerror is None, the walk will continue, but the error will be reported.
+4) followlinks (Optional, Default: False): This boolean parameter determines whether symbolic links to directories should be followed.
+    True: Symbolic links to directories will be followed, meaning the walk will enter the linked directory.
+    False: Symbolic links to directories will be ignored.   
+'''
             
 #To create a Directory/Directories
 os.makedirs('Hello')
@@ -61,3 +74,20 @@ path_var = os.environ.get("PATH")
 print(home_dir)
 print(path_var)
 
+#To print the name of the operating system
+print(os.name)
+
+# This will split the file with root and extensions seperately 
+path=r"C:\Users\ky\Desktop\Python Practise"
+for file in os.listdir(path):
+    print(os.path.splitext(file))
+    
+# Extracts the directory part of a path
+dirname = os.path.dirname("/path/to/file.txt") # Output: /path/to
+
+# To joinng multiple paths
+path = r"\Users\ky\Desktop\Helaoe\jELE"
+path1 = r"Python Practise"
+
+combined_path = os.path.join("C:", path1, path[1:]) #path[1:] removes the first \
+print(combined_path) # Output : C:\Python Practise\Users\ky\Desktop\Helaoe\jELE
